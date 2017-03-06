9fc0fb46ccefd2da65870892b7e3f350960ad612
randomcoww <etherblimp@gmail.com>
2017-03-05 22:37:57 -0800

    add exists property

--- libraries/resource/config.rb
+++ libraries/resource/config.rb
@@ -6,6 +6,7 @@ class ChefKea
       default_action :create
       allowed_actions :create, :delete
 
+      property :exists, [TrueClass, FalseClass]
       property :config, Hash
       property :content, String, default: lazy { to_conf }
       property :path, String, desired_state: false,
@@ -16,7 +17,7 @@ class ChefKea
       end
 
       def path
-        
+
       end
 
       private
