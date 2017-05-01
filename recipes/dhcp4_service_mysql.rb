fd68429c5fe7dcce35c48235402b720042ae47de
randomcoww <etherblimp@gmail.com>
2017-04-30 20:24:31 -0700

    systemd update

---
+++ recipes/dhcp4_service_mysql.rb
@@ -0,0 +1,27 @@
+## rewrite this to include mysql dependency and restart
+systemd_unit "kea-dhcp4-server.service" do
+  content ({
+    'Unit' => {
+      'Description' => 'ISC KEA IPv4 DHCP daemon',
+      'Documentation' => 'man:kea-dhcp4(8)',
+      'After' => [
+        'mysql.service',
+        'network-online.target',
+        'time-sync.target'
+      ],
+    },
+    'Service' => {
+      'Restart' => 'always',
+      'RestartSec' => 5,
+      "ExecStart" => '/usr/sbin/kea-dhcp4 -c /etc/kea/kea-dhcp4.conf'
+    },
+    'Install' => {
+      'WantedBy' => 'multi-user.target'
+    }
+  })
+  action [:create]
+end
+
+service 'kea-dhcp4-server' do
+  action [:enable, :start]
+end
