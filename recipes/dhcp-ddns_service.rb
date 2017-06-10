ab07eb837587d11e62033238a21604f7fd72d651
randomcoww <etherblimp@gmail.com>
2017-06-10 02:22:57 -0700

    add ddns restart always

--- recipes/dhcp-ddns_service.rb
+++ recipes/dhcp-ddns_service.rb
@@ -1,3 +1,15 @@
+systemd_resource_dropin "10-restart.conf" do
+  service "kea-dhcp-ddns-server.service"
+  config ({
+    'Service' => {
+      'Restart' => 'always',
+      'RestartSec' => 5,
+    }
+  })
+  action [:create]
+  # notifies :restart, "service[kea-dhcp4-server]", :delayed
+end
+
 service 'kea-dhcp-ddns-server' do
   action [:enable, :start]
 end
